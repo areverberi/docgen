e54aa4badb86612118556d6441f5e2270d021954
Andrea Reverberi <andrea.reverberi@gatech.edu>
2015-06-07 21:46:38 +0200

    removed unused scripts

--- js/common/reqhandler.js
+++
@@ -1,4 +0,0 @@
-//put in vals all the stuff that's not table or kind (eg, where, join, etc)
-function richiesta(tb, tp, v, section){
-	$.post("/gestionet.php", {tab:tb, tipo:tp, vals:v}, function(data){visualizza(data, section)}, "json");
-}
